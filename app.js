3b0abbef0a3dd8c601657cf5ee473db6ce38bba5
axel Fourreau <fourreauaxel@gmail.com>
2025-09-16 09:48:21 +0200

    Commit#3

--- app.js
+++ app.js
@@ -1,25 +1,64 @@
 'use strict';
 
 /*  *********************** Serveur Web ***************************   */
-// 
+//
 var express = require('express');
 var exp = express();
-var port = 80;
-exp.use(express.static(__dirname + '/www')); 
+exp.use(express.static(__dirname + '/www'));
 
 exp.get('/', function (req, res) {
-    console.log('Reponse a un client'); 
+    console.log('Reponse a un client');
     res.sendFile(__dirname + '/www/index.html');
-}); 
-
+});
 exp.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500).send('Erreur serveur express');
-}); 
+});
 
-exp.listen(80, function () {
+/*  *************** serveur WebSocket express *********************   */
+//
+var expressWs = require('express-ws')(exp);
+
+// Connexion des clients à la WebSocket /echo et evenements associés
+exp.ws('/echo', function (ws, req) {
+
+    console.log('Connection WebSocket %s sur le port %s',
+        req.connection.remoteAddress, req.connection.remotePort);
+
+    ws.on('message', function (message) {
+        console.log('De %s %s, message :%s', req.connection.remoteAddress,
+            req.connection.remotePort, message);
+        ws.send(message);
+    });
+
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion WebSocket %s sur le port %s',
+            req.connection.remoteAddress, req.connection.remotePort);
+    });
+
+});
+
+var portServ = 80;
+exp.listen(portServ, function () {
     console.log('Serveur en ecoute');
-}); 
+});
 
+/*  ****************** Broadcast clients WebSocket  **************   */
+var aWss = expressWs.getWss('/echo');
+var WebSocket = require('ws');
+aWss.broadcast = function broadcast(data) {
+    console.log("Broadcast aux clients navigateur : %s", data);
+    aWss.clients.forEach(function each(client) {
+        if (client.readyState == WebSocket.OPEN) {
+            client.send(data, function ack(error) {
+                console.log("    -  %s-%s", client._socket.remoteAddress,
+                    client._socket.remotePort);
+                if (error) {
+                    console.log('ERREUR websocket broadcast : %s', error.toString());
+                }
+            });
+        }
+    });
+};
 
 console.log('TP CIEL');
